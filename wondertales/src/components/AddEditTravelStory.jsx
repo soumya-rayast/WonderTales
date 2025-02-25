d5d4812be6572e808e0577787a6240668741f7b9
Soumya Rayast <soumya.rayast@enropesolutions.com>
2025-02-25 15:45:14 +0530

    working on story uploading and edit component

--- wondertales/src/components/AddEditTravelStory.jsx
+++ wondertales/src/components/AddEditTravelStory.jsx
@@ -2,20 +2,66 @@ import React, { useState } from 'react'
 import { MdAdd, MdClose, MdDeleteOutline, MdUpdate } from 'react-icons/md'
 import DateSelector from './DateSelector'
 import ImageSelector from './ImageSelector';
+import TagInput from './TagInput';
+import axiosInstance from '../utils/axiosInstance';
+import moment from 'moment';
+import { toast } from 'react-toastify'
+import uploadImage from '../utils/uploadImage';
 
-const AddEditTravelStory = ({
-    storyInfo,
-    type,
-    onClose,
-    getAllTravelStories }) => {
+const AddEditTravelStory = ({ storyInfo, type, onClose, getAllTravelStories }) => {
 
     const [visitedDate, setVisitedDate] = useState(null);
     const [title, setTitle] = useState('');
     const [storyImg, setStoryImg] = useState(null);
     const [story, setStory] = useState("");
     const [visitedLocation, setVisitedLocation] = useState([]);
+    const [error, setError] = useState('')
 
-    const handleAddOrUpdateClick = () => { }
+    const addNewTravelStory = async () => {
+        try {
+            let imageUrl = "";
+            if (storyImg) {
+                const imgUploads = await uploadImage(storyImg);
+                imageUrl = imgUploads.imageUrl || "";
+            }
+            const response = await axiosInstance.post("/add-travel-story", {
+                title,
+                story,
+                imageUrl: imageUrl || "",
+                visitedLocation,
+                visitedDate: visitedDate ? moment(visitedDate).valueOf() : moment().valueOf(),
+            });
+            if (response.data && response.data.story) {
+                toast.success("story added successfully");
+                getAllTravelStories();
+                onClose();
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    const updateTravelStory = () => {
+
+    }
+    const handleAddOrUpdateClick = () => {
+        if (!title) {
+            setError("please enter the title");
+            return;
+        }
+        if (!story) {
+            setError("Please enter the story")
+            return;
+        }
+        setError("")
+
+        if (type === "edit") {
+            updateTravelStory();
+        } else {
+            addNewTravelStory();
+        }
+    };
+    const handleDeleteStoryImg = () => { };
+
 
     return (
         <div>
@@ -26,27 +72,41 @@ const AddEditTravelStory = ({
                 <div>
                     <div className=' flex items-center gap-3 bg-cyan-50/50 p-2 rounded-l-lg'>
                         {type === 'add' ?
-                            <button className='btn-small ' onClick={() => { }}>
+                            <button
+                                className='btn-small '
+                                onClick={() => { }}>
                                 <MdAdd className='text-lg' /> ADD STORY
                             </button> :
                             <>
-                                <button className='btn-small' onClick={handleAddOrUpdateClick} >
+                                <button
+                                    className='btn-small'
+                                    onClick={handleAddOrUpdateClick} >
                                     <MdUpdate className='text-lg' /> UPDATE STORY
                                 </button>
-                                <button className='btn-small btn-delete' onClick={onClose} >
+                                <button
+                                    className='btn-small btn-delete'
+                                    onClick={onClose} >
                                     <MdDeleteOutline className='text-lg' />DELETE
                                 </button>
                             </>
                         }
-                        <button className='' onClick={onClose}>
+                        <button
+                            className=''
+                            onClick={onClose}>
                             <MdClose className="text-xl text-slate-400" />
                         </button>
                     </div>
+                    {
+                        error && (
+                            <p className='text-red-500 text-xs pt-2 text-right'>{error}</p>
+                        )
+                    }
                 </div>
             </div>
             <div>
                 <div className='flex-1 flex flex-col gap-2 pt-4'>
-                    <label className='input-label'>TITLE</label>
+                    <label
+                        className='input-label'>TITLE</label>
                     <input
                         className='text-xl text-slate-950 outline-none'
                         type="text"
@@ -55,9 +115,14 @@ const AddEditTravelStory = ({
                         onChange={({ target }) => setTitle(target.value)}
                     />
                     <div className='my-3'>
-                        <DateSelector date={visitedDate} setDate={setVisitedDate} />
+                        <DateSelector
+                            date={visitedDate}
+                            setDate={setVisitedDate}
+                            handleDeleteImg={handleDeleteStoryImg} />
                     </div>
-                    <ImageSelector image={storyImg} setImage={setStoryImg} />
+                    <ImageSelector
+                        image={storyImg}
+                        setImage={setStoryImg} />
                     <div className='flex flex-col gap-2 mt-4'>
                         <textarea
                             type='text'
@@ -67,6 +132,13 @@ const AddEditTravelStory = ({
                             value={story}
                             onChange={({ target }) => setStory(target.value)}
                         />
+                    </div>
+
+                    <div className='pt-3'>
+                        <label className='input-label'>VISITED LOCATIONS</label>
+                        <TagInput
+                            tags={visitedLocation}
+                            setTags={setVisitedLocation} />
                     </div>
                 </div>
             </div>
--- wondertales/src/components/ImageSelector.jsx
+++ wondertales/src/components/ImageSelector.jsx
@@ -1,16 +1,40 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { FaRegFile } from 'react-icons/fa'
-const ImageSelector = ({ image, setImage }) => {
+import { MdDeleteOutline } from 'react-icons/md'
+const ImageSelector = ({ image, setImage , handleDeleteImg}) => {
   const inputRef = useRef(null);
   const [previewUrl, setPreviewUrl] = useState(null);
 
-  const handleImageChange = () => {
-
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setImage(file)
+    }
   }
 
-  const onChooseFile = () =>{
+  const onChooseFile = () => {
     inputRef.current.click();
   }
+
+  const handleRemoveImage = () => {
+    setImage(null);
+    handleDeleteImg();
+  }
+
+  useEffect(() => {
+    if (typeof image === "string") {
+      setPreviewUrl(image);
+    } else if (image) {
+      setPreviewUrl(URL.createObjectURL(image));
+    } else {
+      setPreviewUrl(null);
+    }
+    return () => {
+      if (previewUrl && typeof previewUrl === 'string' && !image) {
+        URL.revokeObjectURL(previewUrl);
+      }
+    }
+  }, [image]);
 
   return (
     <div>
@@ -21,12 +45,32 @@ const ImageSelector = ({ image, setImage }) => {
         onChange={handleImageChange}
         className='hidden'
       />
-      <button className='w-full flex flex-col items-center justify-center gap-4 bg-slate-50 rounded border border-slate-200/50' onClick={() => onChooseFile()}>
-        <div className='flex w-14 h-14 items-center justify-center bg-cyan-50 rounded-full border-cyan-100'>
-          <FaRegFile className='text-xl text-cyan-500' />
+      {image ? <button
+        className='w-full flex flex-col items-center justify-center gap-4 bg-slate-50 rounded border border-slate-200/50'
+        onClick={() => onChooseFile()}>
+        <div
+          className='flex w-14 h-14 items-center justify-center bg-cyan-50 rounded-full border-cyan-100'>
+          <FaRegFile
+            className='text-xl text-cyan-500' />
         </div>
-        <p className='text-sm text-slate-500'>Browse image files to upload</p>
-      </button>
+        <p
+          className='text-sm text-slate-500'>
+          Browse image files to upload
+        </p>
+      </button> :
+        <div className='w-full relative'>
+          <img
+            src={previewUrl}
+            alt="Selected"
+            className='w-full h-[300px] object-cover rounded-lg' />
+
+          <button
+            onClick={handleRemoveImage}
+            className='btn-small btn-delete absolute top-2 right-2'>
+            <MdDeleteOutline className='text-lg' />
+          </button>
+        </div>
+      }
     </div>
   )
 }
